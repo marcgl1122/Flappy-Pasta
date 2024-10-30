6c0c550e7a799159f023d7c82c6383639c9a57a8
marcgl1122 <marc_glarner@stud.phzh.ch>
2024-10-30 11:45:22 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
 const img = new Image();
-img.src = "https://i.ibb.co/Q9yv5Jk/flappy-bird-set.png";
+img.src = "pasta-sprite.png";
 
 // Allgemeine Einstellungen
 let gamePlaying = false;
